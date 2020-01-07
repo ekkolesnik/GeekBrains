edfcc7f274b1b94092e91f38db1b949ff726e747
Kolesnik Evgeny <43893173+ekkolesnik@users.noreply.github.com>
2020-01-07 19:26:28 +0300

    Переделал ДЗ
    Полностью пересмотрел подход к выполнению ДЗ и переделал его.
    В перечисление теперь включил все возможные действия с авто.
    Погрузка и разгрузка учитывает объём багажника.

--- Lesson_3.playground/Contents.swift
+++ Lesson_3.playground/Contents.swift
@@ -1,97 +1,159 @@
 import UIKit
 
-enum Actions {
-    case startEngine
-    case stopEngine
-    case openWindow
-    case closeWindow
-}
-
-enum Pogruzka {
-    case loading(Int)
-    func shet() -> Int {
-        switch self {
-        case .loading(let num):
-            return num
-        }
+// Создание перечисления со всеми действиями над авто
+enum Actions2 {
+    enum Loa {
+        case loading
+        case unloading
     }
+    
+    enum Engi {
+        case start
+        case stop
+    }
+    
+    enum Wind {
+        case open
+        case close
+    }
+    
+    case engine(name: Engi)
+    case window(name: Wind)
+    case loading(doing: Loa, mass: Int)
 }
 
+//Создание структуры авто
 struct Car {
-    var model = "BMW"
-    var year = 2018
-    var trunkVolume = 180
-    var trunkFilled = 0
-    var engine = "Двигатель остановлен"
-    var windowsOpen = "Окна закрыты"
     
-    mutating func сhangeProperty(property: Actions){
-        if property == .closeWindow {
-            self.windowsOpen = "Окна закрыты"
-        } else if property == .openWindow {
-            self.windowsOpen = "Окна открыты"
-        } else if property == .startEngine {
-            self.engine = "Двигатель запущен"
-        } else if property == .stopEngine {
-            self.engine = "Двигатель остановлен"
-        }
-    }
+    var model: String
+    var year: Int
+    var trunkVolume: Int
+    var trunkFilled: Int
+    var engine: String
+    var windows: String
     
-    mutating func pogruzka(nam: Pogruzka){
-        if nam.shet() > self.trunkVolume {
-            print("\nИзвините, вес слишком большой")
-        } else if nam.shet() <= self.trunkVolume {
-            self.trunkFilled = nam.shet()
+    mutating func сhangeProperty(property: Actions2){
+        switch property {
+        case .engine(name: .start):
+            self.engine = "Start"
+        case .engine(name: .stop):
+            self.engine = "Stop"
+        case let .loading(doing, mass):
+            let tv = trunkVolume
+            let tf = trunkFilled
+            let ma = mass
+            if tf + ma < tv && doing == .loading {
+                trunkFilled = trunkFilled + mass
+            } else if tf + ma == tv && doing == .loading {
+                print("ВНИМАНИЕ: Багажник заполнен!")
+                trunkFilled = trunkFilled + mass
+            } else if tf + ma > tv && doing == .loading {
+                print("Погрузка не возможна, так как будет привышен максимально допустимый вес, равный: \(trunkVolume)")
+            } else if tf - ma < 0 && doing == .unloading {
+                print("В багажнике нет столько груза для его разгрузки")
+            } else if tf - ma > 0 && doing == .unloading {
+                trunkFilled = trunkFilled - mass
+            }
+        case .window(name: .close):
+            self.windows = "Close"
+        case .window(name: .open):
+            self.windows = "Open"
         }
     }
     
     func allProperty(){
-        print("\nОписание авто:\nМодель: \(self.model)\nГод выпуска: \(self.year)\nВместимость багажника: \(self.trunkVolume) л.\nЗаполненность багажника: \(self.trunkFilled)\nДвигатель: \(self.engine)\nОкна (статус): \(self.windowsOpen)")
+        print("\nОписание авто:\nМодель: \(self.model)\nГод выпуска: \(self.year)\nВместимость багажника: \(self.trunkVolume) л.\nЗаполненность багажника: \(self.trunkFilled) л.\nДвигатель: \(self.engine)\nОкна (статус): \(self.windows)")
     }
+    
+    init(model: String, year: Int, trunkVolume: Int) {
+        self.model = model
+        self.year = year
+        self.trunkVolume = trunkVolume
+        self.trunkFilled = 0
+        self.engine = "Stop"
+        self.windows = "Close"
+    }
+    
 }
 
+// Инициализация авто
+var auto = Car.init(model: "BMW", year: 2018, trunkVolume: 300)
+
+// Загрузка/Разгрузка
+auto.сhangeProperty(property: .loading(doing: .loading, mass: 60))
+auto.сhangeProperty(property: .loading(doing: .unloading, mass: 40))
+
+// Изменение свойств авто
+auto.сhangeProperty(property: .engine(name: .start))
+auto.сhangeProperty(property: .window(name: .open))
+
+// Вывод всех показателей в консоль
+auto.allProperty()
+
+// Создание структуры грузовика
 struct Truck {
-    var model = "Scania"
-    var year = 2015
-    var trunkVolume = 3000
-    var trunkFilled = 0
-    var engine = "Двигатель остановлен"
-    var windowsOpen = "Окна закрыты"
     
-    mutating func сhangeProperty(property: Actions){
-        if property == .closeWindow {
-            self.windowsOpen = "Окна закрыты"
-        } else if property == .openWindow {
-            self.windowsOpen = "Окна открыты"
-        } else if property == .startEngine {
-            self.engine = "Двигатель запущен"
-        } else if property == .stopEngine {
-            self.engine = "Двигатель остановлен"
-        }
-    }
+    var model: String
+    var year: Int
+    var trunkVolume: Int
+    var trunkFilled: Int
+    var engine: String
+    var windows: String
     
-    mutating func pogruzka(nam: Pogruzka){
-        if nam.shet() > self.trunkVolume {
-            print("\nИзвините, вес слишком большой")
-        } else if nam.shet() <= self.trunkVolume {
-            self.trunkFilled = nam.shet()
+    mutating func сhangeProperty(property: Actions2){
+        switch property {
+        case .engine(name: .start):
+            self.engine = "Start"
+        case .engine(name: .stop):
+            self.engine = "Stop"
+        case let .loading(doing, mass):
+            let tv = trunkVolume
+            let tf = trunkFilled
+            let ma = mass
+            if tf + ma < tv && doing == .loading {
+                trunkFilled = trunkFilled + mass
+            } else if tf + ma == tv && doing == .loading {
+                print("ВНИМАНИЕ: Грузовой отсек заполнен!")
+                trunkFilled = trunkFilled + mass
+            } else if tf + ma > tv && doing == .loading {
+                print("Погрузка не возможна, так как будет привышен максимально допустимый вес, равный: \(trunkVolume)")
+            } else if tf - ma < 0 && doing == .unloading {
+                print("В грузовом отсеке нет столько груза для его выгрузки")
+            } else if tf - ma > 0 && doing == .unloading {
+                trunkFilled = trunkFilled - mass
+            }
+        case .window(name: .close):
+            self.windows = "Close"
+        case .window(name: .open):
+            self.windows = "Open"
         }
     }
     
     func allProperty(){
-        print("\nОписание авто:\nМодель: \(self.model)\nГод выпуска: \(self.year)\nВместимость груза: \(self.trunkVolume) л.\nЗаполненность багажника: \(self.trunkFilled)\nДвигатель: \(self.engine)\nОкна (статус): \(self.windowsOpen)")
+        print("\nОписание грузовика:\nМодель: \(self.model)\nГод выпуска: \(self.year)\nВместимость грузового отсека: \(self.trunkVolume) л.\nЗаполненность грузового отсека: \(self.trunkFilled) л.\nДвигатель: \(self.engine)\nОкна (статус): \(self.windows)")
     }
+    
+    init(model: String, year: Int, trunkVolume: Int) {
+        self.model = model
+        self.year = year
+        self.trunkVolume = trunkVolume
+        self.trunkFilled = 0
+        self.engine = "Stop"
+        self.windows = "Close"
+    }
+    
 }
 
-var bmw = Car()
-bmw.сhangeProperty(property: .stopEngine)
-bmw.сhangeProperty(property: .openWindow)
-bmw.pogruzka(nam: .loading(300))
-bmw.allProperty()
+// Инициализация авто
+var gruzovik = Truck.init(model: "Scania", year: 2020, trunkVolume: 4000)
 
-var scania = Truck()
-scania.сhangeProperty(property: .startEngine)
-scania.сhangeProperty(property: .openWindow)
-scania.pogruzka(nam: .loading(1500))
-scania.allProperty()
+// Загрузка/Разгрузка
+gruzovik.сhangeProperty(property: .loading(doing: .loading, mass: 2400))
+gruzovik.сhangeProperty(property: .loading(doing: .unloading, mass: 300))
 
+// Изменение свойств авто
+gruzovik.сhangeProperty(property: .engine(name: .start))
+gruzovik.сhangeProperty(property: .window(name: .open))
+
+// Вывод всех показателей в консоль
+gruzovik.allProperty()
